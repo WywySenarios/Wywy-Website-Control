da2f871f22ff1a49c2c25902e1e91946e337097a
WywySenarios <wywyericzhu@gmail.com>
2026-02-02 16:12:45 -0500

    getops, install services

--- install.sh
+++ install.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
 
-if [ -d "/etc/Wywy-Website-Control" ];
-then
-    read -p "Directory /etc/Wywy-Website-Control already exists. Overwrite? [y/n] " overwrite;
-    if ! [[ $overwrite = "y" ]];
-    then
-        echo "Directory /etc/Wywy-Website-Control already exists. Aborting..."
-        exit 1
-    fi
-fi
+y=0
 
+# Check for flags
+while getopts "y" opt;
+do
+    case "${opt}" in
+    y)
+        y=1
+        ;;
+    *)
+        echo "Invalid flag \"-${opt}\". Expected -y for automatically say yes." &>2
+        exit 1
+        ;;
+    esac
+done
+
+# shift args so that position arguments make sense
+shift $((OPTIND-1))
+
+# install the control repo
 sudo mkdir /etc/Wywy-Website-Control
 sudo chmod 755 /etc/Wywy-Website-Control
 sudo chown 755 $USER:$USER /etc/Wywy-Website-Control
-
 git clone git@github.com:WywySenarios/Wywy-Website-Control.git /etc/Wywy-Website-Control
+
+# clone every service that is desired by the user.
+for service_name in (cat /etc/Wywy-Website-Control/services.txt); do
+    read -p "Install service $service_name? [y/n]" overwrite
+    if ! [[ $overwrite = "y" ]] && ! [[ $y = "1" ]]
+    then
+        continue
+    fi
+
+    sudo mkdir /usr/local/Wywy-Website
+    sudo chmod 755 /usr/local/Wywy-Website
+    sudo chown 755 $USER:$USER /usr/local/Wywy-Website
+    git clone git@github.com:WywySenarios/Wywy-Website-$service_name.git /usr/local/Wywy-Website/Wywy-Website-$service_name
+done
+
+# create secrets directory
+mkdir /etc/Wywy-Website-Control/secrets
+chmod 755 /etc/Wywy-Website-Control/secrets
