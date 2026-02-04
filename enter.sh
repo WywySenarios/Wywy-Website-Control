42b01b51d681e77a7d5839753e43e50d1449b03f
WywySenarios <wywyericzhu@gmail.com>
2026-02-03 20:37:21 -0500

    Consolidate enter scripts

--- enter.sh
+++ enter.sh
@@ -1,9 +1,63 @@
 #!/bin/bash
 # Arguments:
 #   $1: Service name (reduced, lower snake case)
-#   ...: Arguments to pass into the service's enter script.
+#   $2: Short-hand container name
 
-target="scripts/enter/$1.sh"
+if [[ -z "$1" || -z "$2" ]]; then
+  echo "Bad arguments. Expected [service name] [short-hand container name]" &>2
+  exit 1
+fi
 
-shift $((OPTIND-1))
-bash "$target" "$@"
+case "$1" in
+  backup)
+    sudo docker exec -it wywywebsite_backup bash
+    ;;
+  cache)
+    case "$2" in
+      sync)
+        sudo docker run -it --rm \
+        -p 2523:2523 \
+        --env-file .env \
+        --mount type=bind,source="$(pwd)/../secrets/admin.txt",target=/run/secrets/admin,readonly \
+        docker-sync /bin/sh
+        ;;
+      mod)
+        sudo docker run -it --rm \
+        -p 2325:2325 \
+        --env-file .env \
+        --mount type=bind,source="$(pwd)/../secrets/admin.txt",target=/run/secrets/admin,readonly \
+        docker-mod /bin/sh
+        ;;
+      pgres)
+        sudo docker run -it --rm -p 5432:5432 -v "postgres-db:/var/lib/postgresql" docker-postgres /bin/sh
+        ;;
+      *)
+        echo "Error: Invalid argument '$1'. Expected 'sync', 'mod', or 'pgres'."
+        exit 1
+        ;;
+    esac
+    ;;
+  website)
+    case "$2" in
+      astro)
+        docker exec -it wywywebsite_astro-dev-server bash
+        ;;
+      sqlr)
+        docker exec -it wywywebsite_sql-receptionist-dev-server bash
+        ;;
+      pgres)
+        docker exec -it wywywebsite_postgres bash
+        ;;
+      create_tables)
+        docker run -it --rm docker-wywywebsite_create_tables bash
+        ;;
+      *)
+        echo "Error: Invalid argument '$2'. Expected 'astro', 'sqlr', or 'pgres'."
+        exit 1
+        ;;
+    esac
+    ;;
+  *)
+    echo "Unknown service name \"$1\"." &>2
+    ;;
+esac
--- scripts/enter/backup.sh
+++
@@ -1,1 +0,0 @@
-sudo docker exec -it wywywebsite_backup bash
