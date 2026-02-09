e71994c5aa11c09a355cfd4122efe1355de66427
WywySenarios <wywyericzhu@gmail.com>
2026-02-09 14:50:04 -0500

    Add -b flag to cache.sh

--- scripts/run/cache.sh
+++ scripts/run/cache.sh
@@ -6,9 +6,27 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
+endflags=""
 project_dir="/usr/local/Wywy-Website/Wywy-Website-Cache"
 docker_dir="$project_dir/docker"
 config_dir="/etc/Wywy-Website-Control/config"
+
+# Check for flags
+while getopts "b" opt;
+do
+    case "${opt}" in
+    b)
+        endflags="${endflags} --build"
+        ;;
+    *)
+        echo "Invalid flag \"-${opt}\". Expected -b for build." &>2
+        exit 1
+        ;;
+    esac
+done
+
+# shift args so that position arguments make sense
+shift $((OPTIND-1))
 
 case "$1" in
     prod)
@@ -16,7 +34,7 @@ case "$1" in
             --env-file "$config_dir/.env" \
             --env-file "$config_dir/cache/.env" \
             --env-file "$config_dir/cache/.env.prod" \
-            up
+            up ${endflags}
         ;;
     dev)
         docker compose -f "$docker_dir/docker-compose.dev.yml" \
@@ -24,7 +42,7 @@ case "$1" in
             --env-file "$config_dir/cache/.env" \
             --env-file "$config_dir/cache/.env.dev" \
             up \
-            --watch
+            --watch ${endflags}
         ;;
     *)
         echo "Error: Invalid argument '$1'. Expected 'prod' or 'dev'"
