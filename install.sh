6b7bf904c566951b1e86f4682faf2aef9e0c4b79
WywySenarios <wywyericzhu@gmail.com>
2026-02-12 11:18:57 -0500

    Suppress mkdir directory already exists errors in install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ done
 shift $((OPTIND-1))
 
 # install the control repo
-sudo mkdir /etc/Wywy-Website-Control
+sudo -p mkdir /etc/Wywy-Website-Control
 sudo chmod 755 /etc/Wywy-Website-Control
 sudo chown $USER:$USER /etc/Wywy-Website-Control
 git clone https://github.com/WywySenarios/Wywy-Website-Control.git /etc/Wywy-Website-Control
@@ -33,16 +33,16 @@ for repo_name in $(cat /etc/Wywy-Website-Control/repos.txt); do
         continue
     fi
 
-    sudo mkdir /usr/local/Wywy-Website
+    sudo -p mkdir /usr/local/Wywy-Website
     sudo chmod 755 /usr/local/Wywy-Website
     sudo chown $USER:$USER /usr/local/Wywy-Website
     git clone https://github.com/WywySenarios/$repo_name.git /usr/local/Wywy-Website/$repo_name
 done
 
 # create secrets directory
-mkdir /etc/Wywy-Website-Control/secrets
+mkdir -p /etc/Wywy-Website-Control/secrets
 chmod 755 /etc/Wywy-Website-Control/secrets
 for service_name in $(cat /etc/Wywy-Website-Control/services.txt | cut -d',' -f1); do
-    mkdir "/etc/Wywy-Website-Control/secrets/$service_name"
+    mkdir -p "/etc/Wywy-Website-Control/secrets/$service_name"
     chmod 755 "/etc/Wywy-Website-Control/secrets/$service_name"
 done
