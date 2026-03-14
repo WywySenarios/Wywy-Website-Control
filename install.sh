139dd7fb1dae58e3130ce71720cd971bcaebc3e8
WywySenarios <wywyericzhu@gmail.com>
2026-03-14 12:37:47 -0400

    Fix install.sh targets

--- install.sh
+++ install.sh
@@ -75,7 +75,7 @@ echo "Pre-flight succeeded. Proceeding with installation."
 sudo mkdir -p /usr/local/Wywy-Website
 sudo chmod 755 /usr/local/Wywy-Website
 sudo chown $USER:$USER /usr/local/Wywy-Website
-for service_name in $(cat /etc/Wywy-Website-Control/services.txt); do
+for service_name in $(cat /etc/Wywy-Website-Control/services.txt | cut -d',' -f1); do
     read -p "Install service $service_name? [y/n]" overwrite
     if [[ ! "$overwrite" =~ ^[Yy]$ ]]; then
         continue
