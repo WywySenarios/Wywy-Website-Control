f5aef9dbf0bfc60a68fbf223f99c6ca09a835b7d
WywySenarios <wywyericzhu@gmail.com>
2026-03-29 12:14:43 -0400

    Set control repo permissions during installation
    * Best effort basis
    * Similar to other source code

--- install.sh
+++ install.sh
@@ -103,6 +103,11 @@ for service_name in $(cat /etc/Wywy-Website-Control/services.txt | cut -d',' -f1
     "/etc/Wywy-Website-Control/scripts/install/$service_name.sh"
 done
 
+# set permissions for the control repository on a best effort basis
+chmod -R u+rw /etc/Wywy-Website-Control 2>/dev/null || true
+chmod -R g-w+r /etc/Wywy-Website-Control 2>/dev/null || true
+chmod -R o-rwx /etc/Wywy-Website-Control 2>/dev/null || true
+
 # lock down secrets folder
 chmod 000 /etc/Wywy-Website-Control/secrets
 sudo chown root /etc/Wywy-Website-Control/secrets
