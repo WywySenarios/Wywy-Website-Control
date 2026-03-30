8820d42ea13a9d7187876eeea2128d285c611e1e
WywySenarios <wywyericzhu@gmail.com>
2026-03-30 17:03:23 -0400

    Opt for docker built-in flags rather than shopt
    The shopt wrapper could not make the generic run script work because of the very strict getopts letter validity and argument ordering.
    
    As per usual, I opt for each service to manage its own flags (even if it means they go unvalidated, and even if some code is repeated).

--- scripts/run/website.sh
+++ scripts/run/website.sh
@@ -3,7 +3,7 @@ project_dir=/usr/local/Wywy-Website/Wywy-Website
 docker_dir="$project_dir/docker"
 config_dir="/etc/Wywy-Website-Control/config"
 
-EXPECTED_FORMAT="Usage: $0 [compose command] <prod | dev> [exec target or alias]?"
+EXPECTED_FORMAT="Usage: $0 [compose command] <prod | dev> [exec target or alias]? ...[docker compose flags]"
 
 compose_files=()
 env_files=(
@@ -12,31 +12,17 @@ env_files=(
 )
 endflags=()
 
-# Check for flags
-while getopts "b" opt;
-do
-    case "${opt}" in
-    b)
-        endflags+=(--build)
-        ;;
-    *)
-        echo "Invalid flag \"-${opt}\". Expected -b for build." >&2
-        exit 1
-        ;;
-    esac
-done
-
-# shift args so that position arguments make sense
-shift $((OPTIND-1))
-
 # Do not validate. The command will fail by itself if this is invalid.
 compose_command=$1
 shift
 development_environment=$1
 shift
-# Do not validate. The command will fail by itself if this is invalid. This will automatically be ignored if the compose command is not exec.
-exec_target=$1
-shift
+if [[ "$compose_command" == "exec" ]]; then
+    # Do not validate. The command will fail by itself if this is invalid. This will automatically be ignored if the compose command is not exec.
+    exec_target=$1
+    shift
+fi
+endflags+=("$@")
 
 if [[  -z "$development_environment" ]]; then
     echo "Error: Missing development environment." >&2
