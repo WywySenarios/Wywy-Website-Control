1f7439068ff81a787fa3031d3090b9f7dfd4084a
WywySenarios <wywyericzhu@gmail.com>
2026-05-22 21:00:35 -0400

    feat: ai.sh default to control repo

--- ai.sh
+++ ai.sh
@@ -2,12 +2,9 @@
 
 service_name=$1
 
-EXPECTED_FORMAT="Bad arguments. Expected [service name]"
-
 if [[ -z "$service_name" ]]; then
-  echo "No service name was provided." >&2
-  echo "Usage: $EXPECTED_FORMAT" >&2
-  exit 1
+  cd /etc/Wywy-Website-Control && opencode
+  exit
 fi
 
 dir_name=$(grep "^$service_name," /etc/Wywy-Website-Control/services.txt | cut -d',' -f2)
